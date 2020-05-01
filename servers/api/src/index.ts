f441c583e356f510bece89e0082c7115b34ddd8c
7wf <7ywolf@gmail.com>
2020-05-01 15:51:12 -0300

    fix(api): handle errors at `application.listen`

--- servers/api/src/index.ts
+++ servers/api/src/index.ts
@@ -5,6 +5,7 @@ const environment = require('./utils/environment')
 
 const application = createApplication()
 
-application.listen(environment.APP_PORT, () => {
+application.listen(environment.APP_PORT, (error: Error) => {
+    if (error) throw error
     console.log(`The application is listening at port ${environment.APP_PORT}.`)
 })
