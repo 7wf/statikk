a048867dee8408b3e96690a081b00721d897cef2
7wf <7ywolf@gmail.com>
2020-05-01 17:23:28 -0300

    feat(api): JWT authorization fastify plugin
    - Creates a JWT authorization `fastify` plugin
    - Setups a type root at `src/types`
    - Adds a environment variable for generating secrets
    - New dependencies: `fastify-jwt` and `fastify-plugin`

--- servers/api/src/main.ts
+++ servers/api/src/main.ts
@@ -1,12 +1,15 @@
 import fastify from 'fastify'
 
 import routes from './routes'
+import authentication from './authentication'
 
 /**
  * Creates an application.
  */
 export default function createApplication() {
     const application = fastify()
+
+    application.register(authentication)
     application.register(routes)
 
     return application
--- servers/api/src/utils/environment.ts
+++ servers/api/src/utils/environment.ts
@@ -2,3 +2,8 @@
  * The port on which the application will be listening
  */
 export const APP_PORT = process.env.APP_PORT || 1337
+
+/**
+ * The port on which the application will be listening
+ */
+export const APP_SECRET = process.env.APP_SECRET || 'secret'
