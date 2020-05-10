aca0762e841300648b165aef4bcc0f06746b0d42
7wf <7ywolf@gmail.com>
2020-05-10 04:33:16 -0300

    feat(api): add typeorm
    Adds TypeORM with postgres driver and creates a docker-compose.yml file, which allows to up a postgres database easily with docker

--- servers/api/src/index.ts
+++ servers/api/src/index.ts
@@ -1,11 +1,33 @@
 require('dotenv').config()
+require('reflect-metadata')
+
+import * as typeorm from 'typeorm'
+import path from 'path'
 
 const createApplication = require('./main').default
 const environment = require('./utils/environment')
 
-const application = createApplication()
+async function main() {
+    const application = createApplication()
 
-application.listen(environment.APP_PORT, (error: Error) => {
-    if (error) throw error
-    console.log(`The application is listening at port ${environment.APP_PORT}.`)
-})
+    await typeorm.createConnection({
+        type: 'postgres',
+
+        host: environment.POSTGRES_HOST,
+        port: environment.POSTGRES_PORT,
+
+        username: environment.POSTGRES_USER,
+        password: environment.POSTGRES_PASSWORD,
+        database: environment.POSTGRES_DB,
+
+        synchronize: true,
+        entities: [path.resolve(__dirname, 'entities', '*.ts')],
+    })
+
+    application.listen(environment.APP_PORT, (error: Error) => {
+        if (error) throw error
+        console.log(`The application is listening at port ${environment.APP_PORT}.`)
+    })
+}
+
+main()
--- servers/api/src/utils/environment.ts
+++ servers/api/src/utils/environment.ts
@@ -7,3 +7,28 @@ export const APP_PORT = process.env.APP_PORT || 1337
  * The port on which the application will be listening
  */
 export const APP_SECRET = process.env.APP_SECRET || 'secret'
+
+/**
+ * The PostgreSQL hostname.
+ */
+export const POSTGRES_HOST = process.env.POSTGRES_HOST
+
+/**
+ * The PostgreSQL password.
+ */
+export const POSTGRES_PORT = process.env.POSTGRES_PORT
+
+/**
+ * The user that will consume data from PostgreSQL.
+ */
+export const POSTGRES_USER = process.env.POSTGRES_USER
+
+/**
+ * The password of the POSTGRES_USER.
+ */
+export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
+
+/**
+ * The target PostgreSQL database.
+ */
+export const POSTGRES_DB = process.env.POSTGRES_DB || 'statikk'
