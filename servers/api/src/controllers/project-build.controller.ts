e1dead484dcec7e672e62a2f5ca1026c5466b286
7wf <7ywolf@gmail.com>
2020-05-22 08:40:00 -0300

    feat(api): allow users to run project builds

--- servers/api/src/controllers/project-build.controller.ts
+++ servers/api/src/controllers/project-build.controller.ts
@@ -1,8 +1,60 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyRequest, FastifyReply, RouteShorthandOptions } from 'fastify'
+import { ServerResponse } from 'http'
+
+import { Project } from '../entities/project'
+
+/**
+ * The shorthand options for `run`.
+ */
+const runShorthand = (fastify: FastifyInstance): RouteShorthandOptions => ({
+    preValidation: [fastify.authenticate],
+    config: {
+        rateLimit: {
+            max: 4,
+            timeWindow: 30 * 60 * 1000,
+        },
+    },
+})
+
+/**
+ * Builds a project.
+ */
+async function run(request: FastifyRequest, reply: FastifyReply<ServerResponse>) {
+    const { sub } = request.user as { sub: number }
+    const project = await Project.findOne(request.query.project, {
+        relations: ['owner'],
+    })
+
+    if (!project) {
+        reply.status(404) // Not Found
+        throw new Error('Project not found.')
+    }
+
+    if (project.owner.id !== sub) {
+        reply.status(401) // Unauthorized
+        throw new Error("You're not allowed to run builds of this project.")
+    }
+
+    await request.amqpChannel.sendToQueue('build', Buffer.alloc(0), {
+        headers: {
+            repository: project.repository_url,
+        },
+    })
+
+    return { running: true }
+}
 
 /**
  * Setups the project builds controller.
  */
-function setup(fastify: FastifyInstance) {}
+function setup(fastify: FastifyInstance) {
+    fastify.post('/builds', runShorthand(fastify), async (request, reply) => {
+        if (fastify.amqpConnection) {
+            return run(request, reply)
+        } else {
+            throw new Error('The build service is disabled.')
+        }
+    })
+}
 
 export default { setup }
