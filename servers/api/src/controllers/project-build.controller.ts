b9c5f2eecc724d4988b72266424b67bd47a4b46b
7wf <7ywolf@gmail.com>
2020-05-29 20:15:52 -0300

    feat(api): allow users to stop project builds
    Projects builds can be stopped by `POST /build/stop`, `project` query is where the ID of the project should be presented at.
    
    The route is protected by JWT token, that means the user must own the project that is being stopped.
    
    Closes #30

--- servers/api/src/controllers/project-build.controller.ts
+++ servers/api/src/controllers/project-build.controller.ts
@@ -47,16 +47,56 @@ async function run(request: FastifyRequest, reply: FastifyReply<ServerResponse>)
 }
 
 /**
+ * The shorthand options for `stop`.
+ */
+const stopShorthand = (fastify: FastifyInstance): RouteShorthandOptions => ({
+    preValidation: [fastify.authenticate],
+})
+
+/**
+ * Stops a running build.
+ */
+async function stop(request: FastifyRequest, reply: FastifyReply<ServerResponse>) {
+    const { sub } = request.user as { sub: number }
+    const project = await Project.findOne(request.query.project, {
+        relations: ['owner'],
+    })
+
+    if (!project) {
+        reply.status(404) // Not Found
+        throw new Error('Project not found.')
+    }
+
+    if (project.owner.id !== sub) {
+        reply.status(401) // Unauthorized
+        throw new Error("You're not allowed to stop builds of this project.")
+    }
+
+    await request.amqpChannel.sendToQueue('builds', Buffer.alloc(0), {
+        headers: {
+            action: 'stop',
+            'repository-id': project.id,
+        },
+    })
+
+    return { stoped: true } // todo is is building?
+}
+
+/**
  * Setups the project builds controller.
  */
 function setup(fastify: FastifyInstance) {
-    fastify.post('/builds', runShorthand(fastify), async (request, reply) => {
-        if (fastify.amqpConnection) {
-            return run(request, reply)
-        } else {
-            throw new Error('The build service is disabled.')
+    if (fastify.amqpConnection) {
+        fastify.post('/builds', runShorthand(fastify), run)
+        fastify.post('/build/stop', stopShorthand(fastify), stop)
+    } else {
+        const serviceUnavailable = async () => {
+            throw new Error('The build service is unavailable')
         }
-    })
+
+        fastify.post('/builds', serviceUnavailable)
+        fastify.post('/build/stop', serviceUnavailable)
+    }
 }
 
 export default { setup }
