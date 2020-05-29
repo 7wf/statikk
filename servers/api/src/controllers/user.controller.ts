41f95a2d082d233fab901c976e0ea37942241e4c
7wf <7ywolf@gmail.com>
2020-05-29 20:55:32 -0300

    feat(api): create route for user profile
    Creates a new route GET /profile that accepts JWT token and replies with user information such as `id` and `name`.

--- servers/api/src/controllers/user.controller.ts
+++ servers/api/src/controllers/user.controller.ts
@@ -1,8 +1,34 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyRequest, RouteShorthandOptions } from 'fastify'
+
+import { User } from '../entities/user'
+
+/**
+ * The shorthand options for GET /profile.
+ */
+const showProfileShorthand = (fastify: FastifyInstance): RouteShorthandOptions => ({
+    preValidation: [fastify.authenticate],
+})
+
+/**
+ * Shows the profile of the authenticated user.
+ */
+async function showProfile(request: FastifyRequest) {
+    const { sub } = request.user as { sub: number }
+    const user = User.findOneOrFail(sub, {
+        select: {
+            id: true,
+            name: true,
+        },
+    })
+
+    return user
+}
 
 /**
  * Setups the user controller.
  */
-function setup(fastify: FastifyInstance) {}
+function setup(fastify: FastifyInstance) {
+    fastify.get('/profile', showProfileShorthand(fastify), showProfile)
+}
 
 export default { setup }
