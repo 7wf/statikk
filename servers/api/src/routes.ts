f777ec69768c9299ae45fe32952b3db398469a35
7wf <7ywolf@gmail.com>
2020-05-29 20:49:54 -0300

    feat(api): create controller for users

--- servers/api/src/routes.ts
+++ servers/api/src/routes.ts
@@ -6,12 +6,16 @@ import SessionController from './controllers/session.controller'
 import ProjectController from './controllers/project.controller'
 import ProjectBuildController from './controllers/project-build.controller'
 
+import UserController from './controllers/user.controller'
+
 /**
  * Setups all routes to a `fastify` application.
  */
 export default async function setupRoutes(fastify: FastifyInstance) {
     SignUpController.setup(fastify)
     SessionController.setup(fastify)
+
+    UserController.setup(fastify)
 
     ProjectController.setup(fastify)
     ProjectBuildController.setup(fastify)
