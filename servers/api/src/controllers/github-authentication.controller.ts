b9f43ddce4f5911ce9e9002cfa7bbc105bf26b6e
7wf <7ywolf@gmail.com>
2020-05-30 18:43:36 -0300

    feat(api): handle GitHub authorization request

--- servers/api/src/controllers/github-authentication.controller.ts
+++ servers/api/src/controllers/github-authentication.controller.ts
@@ -1,10 +1,63 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { FastifyInstance, RouteShorthandOptions, FastifyRequest, FastifyReply } from 'fastify'
 import { ServerResponse } from 'http'
+
+import { GitHubUser } from '../entities/user-github'
+
+import axios from 'axios'
+
+/**
+ * The URL to exchange `code` for a `access_token` inside GitHub.
+ */
+const GITHUB_TOKEN_EXCHANGE = 'https://github.com/login/oauth/access_token'
 
 /**
  * The URL to GitHub OAuth URL.
  */
 let GITHUB_AUTHORIZATION_URL = ''
+
+// The JWT verify options for `handleAuthorization`
+const verifyOptions = {
+    extractToken: (request: FastifyRequest) => request.query.state,
+}
+
+/**
+ * Handle users authorization request.
+ */
+async function handleAuthorization(request: FastifyRequest) {
+    const { code, state } = request.query
+
+    if (!state || !code) throw new Error('Missing `state` or `code` inside query parameters.')
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    const { sub } = (await request.jwtVerify(verifyOptions)) as { sub: number }
+
+    const exchangeResponse = await axios
+        .post(GITHUB_TOKEN_EXCHANGE, {
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code: code,
+            state,
+        })
+        .then((response) => response.data)
+
+    if (!exchangeResponse.access_token) throw new Error('The access token was not provided by GitHub.')
+    const githubUser = await GitHubUser.findOne(sub)
+
+    if (!githubUser) {
+        const githubUserData = {
+            id: sub,
+            token: exchangeResponse.access_token,
+        }
+
+        await GitHubUser.create(githubUserData).save()
+    } else {
+        await GitHubUser.update(githubUser.id, { token: exchangeResponse.access_token })
+    }
+
+    return { success: true }
+}
 
 /**
  * The shorthand options for GET /auth/github.
@@ -39,6 +92,7 @@ function setup(fastify: FastifyInstance) {
     ].join('')
 
     fastify.get('/auth/github', generateGithubAuthorizationShorthand(fastify), generateGithubAuthorizationURL)
+    fastify.get('/auth/github/callback', handleAuthorization)
 }
 
 export default { setup }
